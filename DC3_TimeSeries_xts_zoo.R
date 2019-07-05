7d5bdeaf69eadf1ca6d125d1fcbfb7611ccb6701
salosek <salosek@nppd.com>
2019-07-05 12:09:15 -0500

    datacamp pdfs

--- DC3_TimeSeries_xts_zoo.R
+++ DC3_TimeSeries_xts_zoo.R
@@ -1,17 +1,38 @@
 cat("\014")
 rm(list=ls())
 
+?xts
 # xts and zoo
 library(xts)
 # xts = matrix + time
 # data + an array of times
 
+# xts constructor
 # X <- xts(x, order.by = idx)
+# to deconstruct a time series
 # coredata()
 # index()
 # as.xts()
 
+# Create dates
+dates <- as.Date("2016-01-01") + 0:4
+
+# Create ts_a
+ts_a <- xts(x = c(1:5), order.by = dates)
+
+# Create ts_b
+ts_b <- xts(x = c(1:5), order.by = as.POSIXct(dates))
+
+# Extract the rows of ts_a using the index of ts_b
+ts_a[index(ts_b)]
+
+# Extract the rows of ts_b using the index of ts_a
+ts_b[index(ts_a)]
+
+
 data("sunspots")
+class(sunspots)
+head(sunspots)
 
 sunspots_xts <-as.xts(sunspots)
 class(sunspots_xts)
@@ -21,10 +42,31 @@ tail(sunspots_xts)
 # read.table() read.csv() read.zoo()
 # write.zoo() saveRDS()
 
+# Convert sunspots to xts using as.xts().
+sunspots_xts <- as.xts(sunspots)
+
+# Get the temporary file name
+tmp <- tempfile() 
+
+# Write the xts object using zoo to tmp 
+write.zoo(sunspots_xts, sep = ",", file = tmp)
+
+# Read the tmp file. FUN = as.yearmon converts strings such as Jan 1749 into a proper time class
+sun <- read.zoo(tmp, sep = ",", FUN = as.yearmon)
+
+# Convert sun into xts. Save this as sun_xts
+sun_xts <- as.xts(sun)
+
+?frequency
+
+# ISO 8601:2004
+# YYYY-MM-DDTHH:MM:SS
+
 # subsetting by time ISO 8601:2004 - "YYYY-MM-DDTHH:MM:SS" format
 # time based queries
 
 # Load Fund data
+
 data(edhec, package = "PerformanceAnalytics")
 
 head(edhec["2007-01", 1])
